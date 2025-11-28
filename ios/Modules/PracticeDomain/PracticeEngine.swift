e27161b529c0e52b6fb2e23d451cf76243d04e69
stew.chan@gmail.com <stew.chan@gmail.com>
2025-11-28 14:32:01 -0500

    Revert "Start blocks immediately without preview countdown"
    This reverts commit 7402bfd77b318c0485a84450b5269aaaa7bbad16.

--- ios/Modules/PracticeDomain/PracticeEngine.swift
+++ ios/Modules/PracticeDomain/PracticeEngine.swift
@@ -110,8 +110,8 @@ final class PracticeEngine {
             return
         }
 
-        // Jump straight into the block (no preview countdown)
-        transitionToInBlock(at: index)
+        // Start with preview state (10 seconds)
+        state = .previewBlock(index: index, secondsRemaining: 10)
         isPaused = false
     }
 
